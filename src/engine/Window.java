80a10ac1445387448d1c2a6623a0c8912fb63021
RicardoCQB <ricardo_cqb@hotmail.com>
2020-03-15 17:16:39 +0000

    Added more classes, base for Game Engine

--- src/engine/Window.java
+++ src/engine/Window.java
@@ -1,18 +1,11 @@
 package engine;
 
-import org.lwjgl.*;
-import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
-import org.lwjgl.system.*;
-
-import java.nio.*;
 
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
-import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
-import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
 public class Window {
